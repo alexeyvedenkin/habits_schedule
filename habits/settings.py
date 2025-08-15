abbd109b84aa8b5e14a88ba5cd79c5ea5afc76d5
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-16 02:57:58 +0500

    дополнительные настройки приложения

--- habits/settings.py
+++ habits/settings.py
@@ -137,10 +137,10 @@ SIMPLE_JWT = {
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = TIME_ZONE
