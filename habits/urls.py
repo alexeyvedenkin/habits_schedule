52d7f4c184041402c332ab525b7900944fa1def4
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-17 01:11:37 +0500

    настройка контроллеров и маршрутизации для модели habit

---
+++ habits/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .apps import HabitsConfig
+from .views import HabitListCreateView, HabitRetrieveUpdateDestroyView
+
+
+app_name = HabitsConfig.name
+
+urlpatterns = [
+    path('habits/', HabitListCreateView.as_view(), name='habit-list-create'),  # Список привычек и создание
+    path('habits/<int:pk>/', HabitRetrieveUpdateDestroyView.as_view(), name='habit-detail'),  # Получение, обновление и удаление
+]
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Habit
+from .serializers import HabitSerializer
 
-# Create your views here.
+class HabitListCreateView(generics.ListCreateAPIView):
+    """
+    Представление для получения списка привычек и создания новой привычки.
+    """
+    queryset = Habit.objects.all()  # Получаем все привычки
+    serializer_class = HabitSerializer  # Используем сериализатор для Habit
+
+class HabitRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Представление для получения, обновления и удаления привычки по ID.
+    """
+    queryset = Habit.objects.all()  # Получаем привычки
+    serializer_class = HabitSerializer  # Используем сериализатор для Habit
