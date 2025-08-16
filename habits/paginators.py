012eb4f74b4701258cf61a126fa2077e48a39b2b
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-17 01:42:36 +0500

    настройка пагинации для модели habit

---
+++ habits/paginators.py
@@ -0,0 +1,8 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class CustomPagination(PageNumberPagination):
+    """ Пагинатор для модели Habit """
+    page_size = 5  # Количество объектов на странице
+    page_size_query_param = 'page_size'  # Позволяет пользователю задать размер страницы
+    max_page_size = 10  # Максимально возможный размер страницы
