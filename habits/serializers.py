9c809b40ac68acb9dfd81dab6233cd2b62cda726
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-17 01:05:46 +0500

    настройка сериализатора модели habit

---
+++ habits/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Habit
+
+class HabitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habit  # Используем модель Habit
+        fields = '__all__'  # Включаем все поля модели
