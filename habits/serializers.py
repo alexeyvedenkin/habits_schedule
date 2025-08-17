aed78cb349c62d82ca5147921606a44b473d920c
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-17 18:25:05 +0500

    настройка сериализаторов для модели Habit

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,9 +1,42 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
-from .models import Habit
+from habits.models import Habit
+from habits.validators import CreateHabitValidator, UpdateHabitValidator
+from users.models import User
 
 
-class HabitSerializer(serializers.ModelSerializer):
+class HabitSerializer(ModelSerializer):
     class Meta:
-        model = Habit  # Используем модель Habit
-        fields = "__all__"  # Включаем все поля модели
+        model = Habit
+        fields = "__all__"
+        read_only_fields = ("user",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance:
+            self.validators = [UpdateHabitValidator(instance=self.instance)]
+        else:
+            self.validators = [CreateHabitValidator()]
+
+
+class UserSimpleSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name"]
+
+
+class PublicHabitSerializer(ModelSerializer):
+    user = UserSimpleSerializer(read_only=True)
+
+    class Meta:
+        model = Habit
+        fields = [
+            "id",
+            "time",
+            "action",
+            "is_pleasant",
+            "frequency",
+            "duration",
+            "user",
+        ]
--- users/models.py
+++ users/models.py
@@ -2,20 +2,6 @@ from django.contrib.auth.models import AbstractUser, UserManager as DefaultUserM
 from django.db import models
 
 
-# class UserManager(DefaultUserManager):  # Создаем собственный менеджер
-#     def create_superuser(self, email, chat_id, password=None, **extra_fields):
-#         """Создание суперпользователя с email и chat_id"""
-#         if not email:
-#             raise ValueError("Email обязателен для создания суперпользователя")
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, chat_id=chat_id, **extra_fields)  # Создаем пользователя
-#         user.set_password(password)  # Устанавливаем пароль
-#         user.is_staff = True
-#         user.is_superuser = True
-#         user.save(using=self._db)
-#         return user
-
-
 class User(AbstractUser):
     username = None
 
