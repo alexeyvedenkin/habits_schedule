4b48a0acd140b373824ac9231c44481f509969cc
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-17 14:01:13 +0500

    настройка маршрутизации для получения swt-токенов

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .apps import UsersConfig
 from .views import UserViewSet
@@ -7,9 +8,13 @@ from .views import UserViewSet
 
 app_name = UsersConfig.name
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh"),
+
 ]
+
+urlpatterns += router.urls
