e506ff196cd81de843f152b14dde27a7bfa9f231
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-17 13:26:27 +0500

    настройка контроллеров и маршрутизации для модели User

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("materials/", include("habits.urls", namespace="habits")),
-
+    path("habits/", include("habits.urls", namespace="habits")),
+    path("users/", include("users.urls", namespace="users")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'list']:
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def get_queryset(self):
+        if self.action == 'list':
+            return User.objects.filter(is_public=True)  # Возвращает только публичные привычки
+        return super().get_queryset()  # Возвращает текущего пользователя для остальных действий
