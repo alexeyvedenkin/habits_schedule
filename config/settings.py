fb5e01ff7ac2eebd257df02c0a305822221357f7
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-17 12:41:27 +0500

    изменение модели User, выполнена миграция

--- config/settings.py
+++ config/settings.py
@@ -104,7 +104,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "users.User"
+# AUTH_USER_MODEL = "users.User"
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True)
+    chat_id = models.BigIntegerField(verbose_name="ID чата в Telegram")
+
+    # Изменяем related_name для групп
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',  # Добавляем уникальное имя
+        blank=True,
+        help_text='The groups this user belongs to.'
+    )
+
+    # Изменяем related_name для разрешений
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions_set',  # Добавляем уникальное имя
+        blank=True,
+        help_text='Specific permissions for this user.'
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.email}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
