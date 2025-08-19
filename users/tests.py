1db5bbdf53d8f3d01cdea1d02344d2e07610a6dc
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-20 01:49:48 +0500

    Настройка тестов для регистрации пользователя

--- users/tests.py
+++ users/tests.py
@@ -2,10 +2,34 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import User  # Импортируем модель User
+from .models import User
 
 
-class UserTests(APITestCase):
+class UserAPITestCase(APITestCase):
+    def setUp(self):
+        # Создаем тестового пользователя
+        self.user_data = {
+            "email": "test@example.com",
+            "password": "password123",  # Убедитесь, что пароль соответствует требованиям
+            "chat_id": 1234567890
+        }
+
+    def test_user_creation_and_str(self):
+        # Отправляем запрос на создание пользователя
+        response = self.client.post("/users/register/", self.user_data, format='json')
+
+        # Проверяем, что запрос успешен
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Получаем созданного пользователя из базы данных
+        user = User.objects.get(email=self.user_data["email"])
+
+        # Проверяем корректное строковое представление
+        expected_str = f"{user.first_name} {user.last_name} - {user.email}"
+        self.assertEqual(str(user), expected_str)
+
+
+class UserTestsCase(APITestCase):
 
     def test_user_registration(self):
         # Создаем URL для регистрации пользователя
